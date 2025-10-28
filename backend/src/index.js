0b94909fa699c664261031bf3af3201025a87bf6
Ishan-Gijavanekar <isg.sachin@gmail.com>
2025-10-28 12:35:45 +0530

    Updating the routes of user, category, chatRoom, contract, job, message

--- backend/src/index.js
+++ backend/src/index.js
@@ -15,6 +15,22 @@ app.use(cors());
 app.use(CookieParser());
 app.use(express.urlencoded({ extended: true }));
 
+// Import Routes
+import userRoutes from "./routes/user.js";
+import categoryRoutes from "./routes/category.js";
+import chatRoomRoutes from "./routes/chatRoom.js";
+import contractRoutes from "./routes/contract.js";
+import jobRoutes from "./routes/job.js";
+import messageRoutes from "./routes/message.js";
+
+// User Routes
+app.use("/api/v1/users", userRoutes)
+app.use("/api/v1/categories", categoryRoutes)
+app.use("/api/v1/chatRooms", chatRoomRoutes)
+app.use("/api/v1/contracts", contractRoutes)
+app.use("/api/v1/jobs", jobRoutes)
+app.use("/api/v1/messages", messageRoutes)
+
 
 
 server.listen(port, () => {
